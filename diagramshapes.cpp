9a1679a3020dbde6946e2df9e073021ed673a724
Marten <Marten@homma-clan.nl>
2019-08-13 00:21:34 +0200

    new file:   diagramshapes.cpp 	new file:   diagramshapes.h

---
+++ diagramshapes.cpp
@@ -0,0 +1,69 @@
+#include "diagramshapes.h"
+
+Line::Line(): DiagramItem (){
+
+}
+
+void Line::setHandles(){
+
+}
+void Line::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget){
+
+}
+
+Ellipse::Ellipse(): DiagramItem (){
+
+}
+
+void Ellipse::setHandles(){
+
+}
+void Ellipse::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget){
+
+}
+
+
+Rectangle::Rectangle(): DiagramItem (){
+
+}
+
+void Rectangle::setHandles(){
+
+}
+void Rectangle::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget){
+
+}
+
+Image::Image(): DiagramItem (){
+
+}
+
+void Image::setHandles(){
+
+}
+void Image::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget){
+
+}
+
+Text::Text(){
+
+}
+
+void Text::setHandles(){
+
+}
+void Text::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget){
+
+}
+
+SimpleText::SimpleText(){
+
+}
+
+void SimpleText::setHandles(){
+
+}
+void SimpleText::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget){
+
+}
+
